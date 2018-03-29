cbc60b989e792cfbe787f1d6a20c3d8ddf8490d3
Khaled Medhat <kerbrose@hotmail.com>
2018-03-29 11:58:24 +0200

    #1 adding accounting chart template module

---
+++ l10n_eg/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'EGYPT - Generic Accounting',
+    'author': 'Core BPO',
+    'website': 'http://www.core-bpo.com',
+    'category': 'Localization',
+    'description': """
+Arab Republic of Egypt accounting chart and localization.
+=======================================================
+
+    """,
+    'depends': ['base', 'account'],
+    'data': [
+             'data/account_data.xml',
+             'data/l10n_eg_chart_data.xml',
+             'data/account_chart_template_data.yml',
+    ],
+}
